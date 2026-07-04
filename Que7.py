4056d05bf07fb84ca65d0b9705d8b27f69fd3d11
yusrasheikh963-debug <yusrasheikh963@gmail.com>
2026-07-04 21:55:38 +0530

    Implement Arithmetic Operations

---
+++ Que7.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+a = np.array([10, 20, 30, 40])  # Create two arrays
+b = np.array([1, 2, 3, 4])
+print("\nArray a:", a)
+print("Array b:", b)
+
+print("\nAddition:")   # Addition
+print(a + b)
+
+print("Subtraction:")  # Subtraction
+print(a - b)
+
+print("Multiplication:")   # Multiplication
+print(a * b)
+
+print("Division:")   # Division
+print(a / b)
+
+print("Element-wise Power:")   # Element-wise Power
+print(a ** b)
+
+print("Modulo Operation:")  # Modulo Operation
+print(a % b)
